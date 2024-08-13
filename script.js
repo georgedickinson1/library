28e75f889b75a68c4c9428a43ebdfd4c06cf238e
George Dickinson <georgedickinson5291@outlook.com>
2024-08-13 14:52:21 +0100

    Create basic functionality using JS
    -Add book function
    -Display book title

--- script.js
+++ script.js
@@ -5,12 +5,31 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-}
+};
 
 Book.prototype.info = function() {
     return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
 };
 
-let book1 = new Book("LV", "Catwalk", 347, "not-read");
+function displayBook(book) {
+    let div = document.createElement("div");
+    div.innerHTML = book.title;
+    document.getElementsByClassName("books")[0].appendChild(div);
 
-console.log(book1.info());
+}
+
+function addBookToLibrary() {
+    let title = prompt("Title: ");
+    let author = prompt("Author: ");
+    let pages = prompt("Pages: ");
+    let read = prompt("Have you read?: ");
+
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    displayBook(book);
+    return;
+};
+
+
+
+console.log(myLibrary);
